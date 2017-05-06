1c65a087939c98e74c9f653b28b1fe91f3c8f36d
RUI1806415923 <1806415923@qq.com>
2017-05-06 17:59:29 +0800

    相机权限，上传跑步机序号请求框架(接口暂时为测试)

--- app/src/main/java/so/go2/sharingthegym/PayActivity.java
+++ app/src/main/java/so/go2/sharingthegym/PayActivity.java
@@ -2,6 +2,7 @@ package so.go2.sharingthegym;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -11,9 +12,16 @@ import com.flipboard.bottomsheet.commons.MenuSheetView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+import so.go2.sharingthegym.model.uploadModel;
+import so.go2.sharingthegym.net.ApiService;
+import so.go2.sharingthegym.net.RetrofitClient;
 
 public class PayActivity extends AppCompatActivity {
 
+    private static final String TAG = "MY_TAG";
     @BindView(R.id.sheet_bottom)
     BottomSheetLayout bottomSheet;
 
@@ -29,6 +37,27 @@ public class PayActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pay);
         ButterKnife.bind(this);
         initMenuSheet();
+        //测试
+        test ();
+    }
+
+    private void test() {
+        RetrofitClient.create(ApiService.class)
+                .upload(10)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<uploadModel>() {
+                    @Override
+                    public void accept(uploadModel uploadModel) throws Exception {
+                        Log.d(TAG, "accept: " + uploadModel
+                                .getArticles().get(0).getArticleId());
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        Log.d(TAG, "accept: " + throwable.getMessage());
+                    }
+                });
     }
 
     private void initMenuSheet() {
